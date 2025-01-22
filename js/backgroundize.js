e92a01e50a98ca08bc8358f88c406cfb30dd36bd
xiaochen477 <1002464042@qq.com>
2025-01-22 15:05:42 +0800

    Site updated: 2025-01-22 15:05:41

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -1,12 +1,9 @@
-// 在手机端显示的背景图片链接
-const mobileBgImageUrl = "url('/img/Phone_background.png')";
-
 // 在电脑端显示的背景图片链接
 const desktopBgImageUrl = "url('/img/PC_background.png')";
 
 // 在手机端设置背景图片
 if (window.innerWidth < 768) {
-  document.querySelector('#web_bg').setAttribute('style', `background-image: ${mobileBgImageUrl};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+  document.querySelector('#web_bg').setAttribute('style', `background-image: ${desktopBgImageUrl};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
 } else {
   // 在电脑端设置背景图片
   document.querySelector('#web_bg').setAttribute('style', `background-image: ${desktopBgImageUrl};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
